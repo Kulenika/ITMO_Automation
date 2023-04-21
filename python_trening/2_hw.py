65c8e187f9f079873c45b7adc0b1e28f4f4c47e5
Anna.Nikonova <anna.kulevova@gmail.com>
2023-04-21 20:41:28 +0300

    first commit

---
+++ python_trening/2_hw.py
@@ -0,0 +1,34 @@
+# ЗАДАЧА 1
+def task_1():
+	age: int = 35
+	tester: bool = True
+	countries: List[str] = ['Bulgaria', 'France', 'Finland', 'Estonia', 'Germany', 'Italy']
+	a: float = 10.5
+	language: str = 'english'
+	print(age)
+	print(tester)
+	print(countries)
+	print(a)
+	print(language)
+task_1()
+
+
+
+# ЗАДАЧА 2
+def task_2():
+	a = [1, 2, 3, 5, 8, 13, 21]
+	print(a[:3])
+task_2()
+
+# ЗАДАЧА 3
+def task_3(x):
+	return x ** 2
+print(task_3(int(input('введите число\n'))))
+
+
+
+
+
+
+
+
