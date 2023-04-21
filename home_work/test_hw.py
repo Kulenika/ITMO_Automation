f17fb0f73e7c0465b3df94f8402d666967b9cf7a
Anna.Nikonova <anna.kulevova@gmail.com>
2023-04-21 20:42:54 +0300

    first commit

---
+++ home_work/test_hw.py
@@ -0,0 +1,8 @@
+def test_passing():
+    assert ('home', 'work') == ('home', 'work')
+
+def test_passing():
+    assert 'QA' == 'QA'
+
+def test_passing():
+    assert not (1, 1, 2, 3, 5) == (2, 3, 5)
